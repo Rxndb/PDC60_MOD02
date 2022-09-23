5b2ed1fdb9c0cd88b37d1dc7f5a0c16b8d574cb4
clvnknt <p.calvinkent@gmail.com>
2022-09-23 12:45:59 +0800

    Add project files.

---
+++ PDC60_MOD02/PDC60_MOD02/View/PageView1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using PDC60_MOD02.ViewModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC60_MOD02.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageView1 : ContentPage
+    {
+        public PageView1()
+        {
+            InitializeComponent();
+            BindingContext = new StudentInformationViewModel();
+        }
+    }
+}
---
+++ PDC60_MOD02/PDC60_MOD02/ViewModel/StudentInformationViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using PDC60_MOD02.Model;
+using Xamarin.Forms;
+
+namespace PDC60_MOD02.ViewModel
+{
+    class StudentInformationViewModel:INotifyPropertyChanged
+    {
+        public StudentInformation StudentInfo { get; set; }
+
+        public Command CommandInformationUpdate { get; set; }
+
+
+        public int sID { get; set; }
+        public string sName { get; set; }
+        public string yLevel { get; set; }
+        public string sec { get; set; }
+        public string cCode { get; set; }
+        public string cour { get; set; }
+
+        public StudentInformationViewModel()
+        {
+            CommandInformationUpdate = new Command(() =>
+            {
+                StudentInfo = new StudentInformation
+                {
+                    StudentID = sID,
+                    StudentName = sName,
+                    YearLevel = yLevel,
+                    Section = sec,
+                    CourseCode = cCode,
+                    Course = cour
+                };
+                OnPropertyChanged(nameof(StudentInfo));
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
